3e72999f968a334d1af891caab0f4f0c9a3abc72
Abu Bakar Siddique <ab.siddique@brainstation23.com>
2021-08-01 15:59:18 +0600

    Add project files.

---
+++ ExpenseManagement.Data/Entity/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpenseManagement.Data.Entity
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        // DbSet
+    }
+}
